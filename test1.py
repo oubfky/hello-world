566752052b9504703546b4a5fc312ef43cf7526e
oubfky <luping53@gmail.com>
2022-11-13 17:31:53 +0800

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    x = np.array([1,2,3,4,5])
+    y = np.array([3,5,2,1,2])
+    plt.scatter(x=x, y=y)
+    plt.show()
